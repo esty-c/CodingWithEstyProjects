42d06defff5b1f725320d959c0a6272aa384f3b5
esty_c <esty_c@live.com>
2020-03-04 02:14:24 +0600

    Asp.NetCoreVersioning repo added
    Signed-off-by: esty_c <esty_c@live.com>

--- Asp.NetCoreVersioning/Asp.NetCoreVersioning/Startup.cs
+++ Asp.NetCoreVersioning/Asp.NetCoreVersioning/Startup.cs
@@ -64,7 +64,7 @@ namespace Asp.NetCoreVersioning
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
